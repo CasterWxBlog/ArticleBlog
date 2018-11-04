44e01efceaf76b82bc6dfba81327a271e5aabb22
CasterWx <1325200471@qq.com>
2018-11-04 21:54:47 +0800

    add shiro

--- src/main/java/com/blog/shiro/Shiro.java
+++ src/main/java/com/blog/shiro/Shiro.java
@@ -1,4 +1,22 @@
 package com.blog.shiro;
 
+import org.apache.shiro.subject.Subject;
+import org.junit.jupiter.api.Test;
+
 public class Shiro {
+    @Test
+    // 权限
+    public void demo3(){
+        Subject subject = ShiroUtil.login("classpath:shiro/shiro.ini","wang","123456") ;
+        //用户验证
+        System.out.println(subject.hasRole("role1"));
+        System.out.println(subject.isPermitted("user:select"));
+        System.out.println(subject.isPermitted("user:add"));
+    }
+
+    @Test
+    // 赋予
+    public void demo(){
+        Subject subject ;
+    }
 }
--- src/main/java/com/blog/shiro/ShiroUtil.java
+++ src/main/java/com/blog/shiro/ShiroUtil.java
@@ -1,4 +1,31 @@
 package com.blog.shiro;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
 public class ShiroUtil {
+    public static Subject login(String configFile,String username,String password){
+        // 读取配置文件，初始化FactoryManager工厂
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory(configFile);
+        // 获取securityManager实例
+        SecurityManager securityManager = factory.getInstance()  ;
+        // 把securityManager实例绑定到SecurityUtils
+        SecurityUtils.setSecurityManager(securityManager);
+        // 得到当前用户
+        Subject subject = SecurityUtils.getSubject() ;
+        // 创建token令牌，用户名/密码
+        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+        try{
+            subject.login(token);
+            System.out.println("login ok");
+        }catch (Exception e){
+            System.out.println(e);
+            System.out.println("login error");
+        }
+        return subject ;
+    }
 }
