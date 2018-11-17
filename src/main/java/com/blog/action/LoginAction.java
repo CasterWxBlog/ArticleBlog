7484b462b50bff9c227e215164b4bb6f5a88deba
CasterWx <1325200471@qq.com>
2018-11-17 13:29:15 +0800

    add main

--- src/main/java/com/blog/action/LoginAction.java
+++ src/main/java/com/blog/action/LoginAction.java
@@ -1,10 +1,12 @@
 package com.blog.action;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 
 public class LoginAction extends HttpServlet {
     @Override
